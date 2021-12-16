60f9295d05b326e59501a11f895af181e322340b
hendra <hendra.wong@inflection.com>
2021-12-15 21:14:26 -0800

    Check in Single Responsibility principle.

---
+++ solid-series/1_single_resposibility/Program.cs
@@ -0,0 +1,50 @@
+﻿var journal = new Journal();
+journal.Add("My Journal #1");
+journal.Add("My Journal #2");
+journal.Add("My Journal #3");
+
+var dbContext = new DbContext();
+dbContext.SaveToDb(journal);
+
+var logger = new Logger();
+logger.WriteLog(journal);
+
+public class Journal
+{
+	private List<string> _entries = new List<String>();
+
+	public override string ToString()
+	{
+		return string.Join(", ", _entries);
+	}
+
+	public int Count
+	{
+		get { return _entries.Count;  }
+	}
+
+	public void Add(string entry)
+	{
+		_entries.Add(entry);
+	}
+
+}
+
+public class DbContext
+{
+	public void SaveToDb(Journal journal)
+	{
+		// Dao/DbContext
+		Console.WriteLine($"Save {journal.Count} records to database");
+	}
+}
+
+public class Logger
+{
+	public void WriteLog(Journal journal)
+	{
+		// Logger
+		Console.WriteLine($"Log: {journal}");
+	}
+
+}
